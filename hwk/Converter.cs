238a3c725d7e041a4e8d61b8df4dd35a5ec24f42
nealbity <yezhou2@outlook.com>
2015-12-13 20:04:18 +0800

    V1.0

---
+++ hwk/Converter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace hwk
+{
+   public  class iConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString() + "牌";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ hwk/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace hwk
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        public class Card
+        {
+            public string Name { get; set; }
+        }
+
+        public ObservableCollection<Card> mcarobservableCollection = new ObservableCollection<Card>();
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            if (he.Text == "") { } 
+            else
+            {
+                mcarobservableCollection.Add(new Card { Name = he.Text });
+                mListView.ItemsSource = mcarobservableCollection;
+            }
+
+        }
+
+        private async void mListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            await new MessageDialog(((Card)e.ClickedItem).Name).ShowAsync();
+        }
+
+    }
+}
